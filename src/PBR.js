d8deb95db4474ab6738521675be832ebbce130bf
Nathan Gordon <gordonnl@gmail.com>
2022-03-06 00:36:21 +1100

    always output RGBM

--- src/PBR.js
+++ src/PBR.js
@@ -1,5 +1,18 @@
-import {Renderer, Transform, Camera, Orbit, Geometry, Sphere, Cube, Texture, Program, Mesh, Color, Vec3} from '../ogl/OGL.js';
-import {convertGLSL}  from './GLSLVersion.js';
+import {
+    Renderer,
+    Transform,
+    Camera,
+    Orbit,
+    Geometry,
+    Sphere,
+    Cube,
+    Texture,
+    Program,
+    Mesh,
+    Color,
+    Vec3,
+} from '../ogl/OGL.js';
+import { convertGLSL } from './GLSLVersion.js';
 import PBRShader from './shaders/PBRShader.js';
 import BackgroundShader from './shaders/BackgroundShader.js';
 
@@ -11,12 +24,12 @@ const OUTPUT_SIZE = 512;
 // RGBD == 3
 let INPUT_TYPE = 0;
 
-const renderer = new Renderer({dpr: 2});
+const renderer = new Renderer({ dpr: 2 });
 renderer.setSize(OUTPUT_SIZE, OUTPUT_SIZE);
 const gl = renderer.gl;
 document.body.appendChild(gl.canvas);
 
-const camera = new Camera(gl, {fov: 35});
+const camera = new Camera(gl, { fov: 35 });
 camera.position.set(2, 0.5, 3);
 
 // Create controls and pass parameters
@@ -26,7 +39,7 @@ const controls = new Orbit(camera, {
 
 function resize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.perspective({aspect: gl.canvas.width / gl.canvas.height})
+    camera.perspective({ aspect: gl.canvas.width / gl.canvas.height });
 }
 window.addEventListener('resize', resize, false);
 resize();
@@ -37,27 +50,27 @@ const program = new Program(gl, {
     vertexShader: PBRShader.vertex,
     fragmentShader: PBRShader.fragment,
     uniforms: {
-        uBaseColor: {value: new Color([1, 1, 1])},
-        
-        tRMO: {value: getTexture('src/assets/materials/hammered-metal-mro.jpg')},
-        uRoughness: {value: 1},
-        uMetallic: {value: 1},
-        uOcclusion: {value: 1},
-        
-        tNormal: {value: getTexture('src/assets/materials/hammered-metal-normal.jpg')},
-        uNormalScale: {value: 2},
-        uNormalUVScale: {value: 3},
-        
-        tLUT: {value: getTexture('src/assets/lut.png', false)},
-        
-        tEnvDiffuse: {value: getTexture('src/assets/interior-diffuse-RGBM.png', false)},
-        tEnvSpecular: {value: getTexture('src/assets/interior-specular-RGBM.png', false)},
-        uEnvSpecular: {value: 1.0},
+        uBaseColor: { value: new Color([1, 1, 1]) },
 
-        uInputType: {value: 2},
-        
-        uLightDirection: {value: new Color([1, 1, 1])},
-        uLightColor: {value: new Vec3(1)},
+        tRMO: { value: getTexture('src/assets/materials/hammered-metal-mro.jpg') },
+        uRoughness: { value: 1 },
+        uMetallic: { value: 1 },
+        uOcclusion: { value: 1 },
+
+        tNormal: { value: getTexture('src/assets/materials/hammered-metal-normal.jpg') },
+        uNormalScale: { value: 2 },
+        uNormalUVScale: { value: 3 },
+
+        tLUT: { value: getTexture('src/assets/lut.png', false) },
+
+        tEnvDiffuse: { value: getTexture('src/assets/interior-diffuse-RGBM.png', false) },
+        tEnvSpecular: { value: getTexture('src/assets/interior-specular-RGBM.png', false) },
+        uEnvSpecular: { value: 1.0 },
+
+        uInputType: { value: 2 },
+
+        uLightDirection: { value: new Color([1, 1, 1]) },
+        uLightColor: { value: new Vec3(1) },
     },
 });
 
@@ -66,12 +79,12 @@ async function loadShaderBall() {
     const data = await (await fetch(`src/assets/shaderball.json`)).json();
 
     const geometry = new Geometry(gl, {
-        position: {size: 3, data: new Float32Array(data.position)},
-        uv: {size: 2, data: new Float32Array(data.uv)},
-        normal: {size: 3, data: new Float32Array(data.normal)},
+        position: { size: 3, data: new Float32Array(data.position) },
+        uv: { size: 2, data: new Float32Array(data.uv) },
+        normal: { size: 3, data: new Float32Array(data.normal) },
     });
 
-    const mesh = new Mesh(gl, {geometry, program});
+    const mesh = new Mesh(gl, { geometry, program });
     mesh.position.y = -0.5;
     mesh.setParent(scene);
 }
@@ -82,19 +95,19 @@ const bgProgram = new Program(gl, {
     vertexShader: convertGLSL(gl, BackgroundShader.vertex),
     fragmentShader: convertGLSL(gl, BackgroundShader.fragment),
     uniforms: {
-        tMap: {value: getTexture('src/assets/interior-diffuse-RGBM.png', false)},
+        tMap: { value: getTexture('src/assets/interior-diffuse-RGBM.png', false) },
 
-        uInputType: {value: 2},
+        uInputType: { value: 2 },
     },
     cullFace: gl.FRONT,
 });
 
-const background = new Mesh(gl, {geometry: bgGeometry, program: bgProgram});
+const background = new Mesh(gl, { geometry: bgGeometry, program: bgProgram });
 background.scale.set(5);
 background.setParent(scene);
 
 function getTexture(src, generateMipmaps = true) {
-    const texture = new Texture(gl, {generateMipmaps});
+    const texture = new Texture(gl, { generateMipmaps });
     texture.wrapS = texture.wrapT = gl.REPEAT;
     const image = new Image();
     image.onload = () => {
@@ -106,19 +119,18 @@ function getTexture(src, generateMipmaps = true) {
 }
 
 export function initPBR() {
-
     animate();
     function animate() {
         requestAnimationFrame(animate);
-        
+
         controls.update();
-        renderer.render({scene, camera});
+        renderer.render({ scene, camera });
     }
 }
 
 export function updateIBL(specularData, diffuseData, inputType) {
-    const diffuse = getTexture(URL.createObjectURL(new Blob([diffuseData], {type: 'image/png'})), false);
-    const specular = getTexture(URL.createObjectURL(new Blob([specularData], {type: 'image/png'})), false);
+    const diffuse = getTexture(URL.createObjectURL(new Blob([diffuseData], { type: 'image/png' })), false);
+    const specular = getTexture(URL.createObjectURL(new Blob([specularData], { type: 'image/png' })), false);
 
     bgProgram.uniforms.tMap.value = diffuse;
     bgProgram.uniforms.uInputType.value = inputType;
--- src/Specular.js
+++ src/Specular.js
@@ -1,4 +1,4 @@
-import {Renderer, Plane, Texture, RenderTarget, Program, Mesh} from '../ogl/OGL.js';
+import { Renderer, Plane, Texture, RenderTarget, Program, Mesh } from '../ogl/OGL.js';
 import SpecularShader from './shaders/SpecularShader.js';
 import AtlasShader from './shaders/AtlasShader.js';
 
@@ -11,7 +11,7 @@ let OUTPUT_SIZE = 512;
 let INPUT_TYPE = 1;
 let OUTPUT_TYPE = 2;
 
-const renderer = new Renderer({alpha: true, premultipliedAlpha: true});
+const renderer = new Renderer({ alpha: true, premultipliedAlpha: true });
 renderer.setSize(OUTPUT_SIZE, OUTPUT_SIZE);
 export const gl = renderer.gl;
 gl.clearColor(0, 0, 0, 0);
@@ -23,26 +23,26 @@ const specularProgram = new Program(gl, {
     vertexShader: SpecularShader.vertex,
     fragmentShader: SpecularShader.fragment,
     uniforms: {
-        tMap: {value: null},
-        uRoughness: {value: 0},
-        uInputType: {value: 0},
-        uOutputType: {value: 0},
+        tMap: { value: null },
+        uRoughness: { value: 0 },
+        uInputType: { value: 0 },
+        uOutputType: { value: 0 },
     },
     transparent: true,
 });
 
-const specularMesh = new Mesh(gl, {geometry, program: specularProgram});
+const specularMesh = new Mesh(gl, { geometry, program: specularProgram });
 
 const atlasProgram = new Program(gl, {
     vertexShader: AtlasShader.vertex,
     fragmentShader: AtlasShader.fragment,
     uniforms: {
-        tMap: {value: null},
+        tMap: { value: null },
     },
     transparent: true,
 });
 
-const atlasMesh = new Mesh(gl, {geometry, program: atlasProgram});
+const atlasMesh = new Mesh(gl, { geometry, program: atlasProgram });
 
 export function renderSpecular(data) {
     INPUT_TYPE = data.inputType;
@@ -63,7 +63,7 @@ export function renderSpecular(data) {
     // Create RenderTargets for each roughness level
     const targets = [];
 
-    for (var i = 0; i < num; i ++) {
+    for (var i = 0; i < num; i++) {
         const target = new RenderTarget(gl, {
             width: size,
             height: size / 2,
@@ -79,27 +79,27 @@ export function renderSpecular(data) {
     specularProgram.uniforms.tMap.value = texture;
     specularProgram.uniforms.uInputType.value = INPUT_TYPE;
     specularProgram.uniforms.uOutputType.value = OUTPUT_TYPE;
-    
+
     // randiance maps for specular
     targets.forEach((target, i) => {
         renderer.setSize(target.width, target.height);
-        
+
         var r = i / (targets.length - 1) || 0;
         specularProgram.uniforms.uRoughness.value = r * 0.9;
 
-        renderer.render({scene: specularMesh, target});
+        renderer.render({ scene: specularMesh, target });
         specularProgram.uniforms.tMap.value = target.texture;
 
         if (i === 0) specularProgram.uniforms.uInputType.value = specularProgram.uniforms.uOutputType.value;
     });
 
     // Update atlas textures
-    atlasProgram.uniforms.tMap.value = targets.map(target => target.texture);
-    
+    atlasProgram.uniforms.tMap.value = targets.map((target) => target.texture);
+
     // Render all targets to atlas output
     renderer.setSize(OUTPUT_SIZE, OUTPUT_SIZE);
 
     // Need to render twice when on external server - I have no idea why
-    renderer.render({scene: atlasMesh});
-    renderer.render({scene: atlasMesh});
+    renderer.render({ scene: atlasMesh });
+    renderer.render({ scene: atlasMesh });
 }
